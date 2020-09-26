ef74d44a52b7b3b68cb5e7953eece31fa14838f0
Arav Narula <media.trouge@gmail.com>
2020-09-26 11:19:33 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,30 @@
+const Discord = require("discord.js");
+const config = require("./config.json");
+
+const client = new Discord.Client()
+
+const prefix = "@";
+
+client.on("message", function(message) {
+
+if (message.author.bot) return;
+if (!message.content.startsWith(prefix)) return;
+
+const commandBody = messafe.content.slice(prefrix.length);
+const args = commandBody.spilt('');
+const command = args.shift().toLowerCase();
+
+                               
+  if (command === "ping") {
+    const timeTaken = Date.now() - message.createdTimestamp;
+    message.reply(`Pong! This message had a latency of ${timeTaken}ms.`);
+  }
+
+ if (command === "about") {
+    message.reply(`This Bot is for Rag3 clan to mange it's server, Profiles and Make the Server a Better Place`)
+    message.reply(`This Bot was built by @Arav Narula#1673!`)
+  }
+
+
+client.login(config.BOT_TOKEN);
+
